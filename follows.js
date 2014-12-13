2c4afe761a109ceb2df608e8aa746025442244ea
michalkorba <michal.korba@iwisher.pl>
2014-12-13 18:20:42 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'LWzfg7qXxOUNmtHuOlpvg4JMxM1ZWdsPdJ8cFT6P6Dyggdtc', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1740911401], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
